083eb4000363552f0b0b46adc154faf8f04d06fe
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-06-19 14:38:58 +0300

    Add Step4 with the program

---
+++ Step4/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача:
+Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] ChangeArray(string[] inArray)
+{
+    string[] res = new string[inArray.Length];
+    int j = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3)
+        {
+            res[j] = inArray[i];
+            j++;
+        }
+    }
+    return res;
+}
+
+string[] a = { "high", "low", "left", "right", "now", "map" };
+string[] b = { "summer", "winter", "spring", "fall" };
+string[] c = { "1234", "432", "34", "-12", "10" };
+
+Console.WriteLine(String.Join(" ", ChangeArray(a)));
+Console.WriteLine(String.Join(" ", ChangeArray(b)));
+Console.WriteLine(String.Join(" ", ChangeArray(c)));
